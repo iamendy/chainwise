aca771ea015f2a9bffcf3ea0deaa8d7ab861ae71
Nnamdi <17215830+iamendy@users.noreply.github.com>
2023-08-11 11:13:02 +0100

    Add create campaign page

--- packages/react-app/components/icons/index.ts
+++ packages/react-app/components/icons/index.ts
@@ -3,5 +3,23 @@ import ChevronUp from "./ChevronUp";
 import ChevronDown from "./ChevronDown";
 import Close from "./Close";
 import Star from "./Star";
+import Home from "./Home";
+import Cart from "./Campaign";
+import ChevronRight from "./ChevronRight";
+import Milestone from "./Milestone";
+import Add from "./Add";
+import Trash from "./Trash";
 
-export { Menu, ChevronUp, ChevronDown, Close, Star };
+export {
+  Menu,
+  ChevronUp,
+  ChevronDown,
+  Close,
+  Star,
+  Home,
+  Cart,
+  ChevronRight,
+  Milestone,
+  Add,
+  Trash,
+};
